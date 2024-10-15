2c170479e4f072c5b96d411be47569ead6c41522
Scott <pessiartist@gmail.com>
2024-10-15 17:09:11 +0200

    Updated files with more functions

--- src/app.py
+++ src/app.py
@@ -1,24 +1,29 @@
-# Made by najasnake12
-# also known as the official -10x developer.
-# Shoutout to Chris Sawyer
-
-
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import hashlib
+from flask_session import Session
+import os
+from PIL import Image
+import time
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
-users_file = 'C:/Users/pessi/OneDrive/Desktop/Social Media For Keyboards/src/instance/users.txt' # For now the database is in a txt file, probably going to change this later
+users_file = 'C:/Users/pessi/OneDrive/Desktop/Social Media For Keyboards/Keeb-Life/src/instance/users.txt'
+img_folder = 'C:/Users/pessi/OneDrive/Desktop/Social Media For Keyboards/Keeb-Life/src/static/images'
+posts = 'C:/Users/pessi/OneDrive/Desktop/Social Media For Keyboards/Keeb-Life/src/instance/posts.txt'
 
-# Routes
 @app.route('/')
 def index():
     return render_template('feed.html')
 
 @app.route('/feed')
 def feed():
-    return render_template('feed.html')
-
+    pass
+    # show the posts to the feed :)
+    # remember to pass the variable to the template
+    
 @app.route('/signup_page', methods=['GET', 'POST'])
 def signup_page():
     try:
@@ -30,19 +35,16 @@ def signup_page():
                 try:
                     with open(users_file, 'a') as file:
                         sha256_hash = hashlib.sha256()
-    
                         sha256_hash.update(password.encode('utf-8'))
-                        
                         hashed_password = sha256_hash.hexdigest()
-                        
-                        file.write(f'{username},{hashed_password}')
+                        file.write(f'{username},{hashed_password}\n')  # Add newline to avoid writing all users on one line
                 except BaseException as e:
                     print(f'Error: {e}')
                     return render_template('error.html')
-                    # maybe also give them a small message that they signed up?
-                return redirect(url_for('login_page')) # Redirect to login if the user signed up succesfully
+
+                return redirect(url_for('login_page'))
             else:
-                return redirect(url_for('signup_page')) # For now, if the the username and or password was empty redirect to signup again.
+                return redirect(url_for('signup_page')) 
     except BaseException as e:
         print(f'Error: {e}')
         return render_template('error.html')
@@ -56,15 +58,15 @@ def login_page():
         password = request.form['password']
         
         try:
-            with open(users_file, 'r') as file:
-                
+            with open(users_file, 'r', encoding='utf-8') as file:
                 for line in file:
                     stored_username, stored_password = line.strip().split(',')
-                    
                     check_if_user_entered_the_correct_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
 
                     if stored_username == username and stored_password == check_if_user_entered_the_correct_password:
-                        return render_template('my_account.html')
+                        session["username"] = username  # create the session if the user logged in
+                        # Perhaps give the user a message if they logged in with no issues?
+                        return redirect(url_for('my_account'))
                 else:
                     return render_template('error1.html')
                     
@@ -76,11 +78,55 @@ def login_page():
 
 @app.route('/my_account')
 def my_account():
-    return render_template('my_account.html')
+    if "username" in session:
+        return render_template('my_account.html', username=session["username"])  # pass the username to the template
+    else:
+        return render_template('error.html')
 
-@app.route('/post')
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)  # clear the session
+    return redirect(url_for('index'))
+
+@app.route('/post', methods=['GET', 'POST'])
 def post():
+    title = ""
+    description = ""
+    resized_image = None
+    if request.method == 'POST':
+        try:
+            title = request.form['title']
+            description = request.form['description']
+            image = request.files.get('image')
+            
+            if image:
+                    
+                # extract the image name and extension
+                image_name, file_extension = os.path.splitext(image.filename)
+                image_name = f"{image_name}_{int(time.time())}{file_extension}"
+                image_path = os.path.join(img_folder, image_name)
+
+                # resize the image
+                open_image = Image.open(image)
+                dimension = (1000, 1000)
+                resized_image = open_image.resize(dimension)
+                resized_image.save(image_path)  # save the resized image
+                
+                with open(posts, 'a') as file:
+                    file.write(f'{title},{description},{image_name}\n')
+            
+            else:
+                with open(posts, 'a') as file:
+                    file.write(f'{title},{description},no_image\n')
+                
+        except BaseException as e:
+            print(f'Error: {e}')
+            return render_template('error.html')
+    
     return render_template('post.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
